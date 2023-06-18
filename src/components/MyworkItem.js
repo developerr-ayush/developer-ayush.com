1e269f3a019e106c8c91bcd93a639089a3ad62ce
shahayush480 <shahayush480@gmail.com>
2023-06-18 07:30:21 +0530

    changes pushed

--- src/components/MyworkItem.js
+++ src/components/MyworkItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function MyworkItem({ MyworkItems }) {
-    
+
     return (
         <div className="workShowing red" data-aos="fade-up" data-aos-duration="500" data-aos-delay="400">
-            <div className="imageWrapWork" style={{backgroundImage:`url(${MyworkItems.url}) `}}>
+            <div className="imageWrapWork" style={{ backgroundImage: `url(${MyworkItems.url}) ` }}>
 
                 {/* <img src={MyworkItems.url} alt="" /> */}
             </div>
@@ -13,20 +13,20 @@ export default function MyworkItem({ MyworkItems }) {
                 <p>
                     <b>Technologies used : </b>
 
-                    
+
 
                     {
-                        MyworkItems.skillsUsed.map(function(elements){
+                        MyworkItems.skillsUsed.map(function (elements) {
                             return (<span key={elements}>{elements}</span>)
                         })
                     }
-                    
+
 
                 </p>
                 <p>
                     {MyworkItems.detail}
                 </p>
-                <a href={MyworkItems.redirectLink} taarget="_blank">View Layout</a>
+                <a href={MyworkItems.redirectLink} target="_blank">View Layout</a>
             </div>
         </div>
     )
