8cc13cf5de935d9a82e98ec73dae1756c4dd2888
shahayush480 <shahayush480@gmail.com>
2024-01-08 07:36:46 +0530

    v3 header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,29 +1,27 @@
-"use client"
 import Image from 'next/image'
 import logo from "@/assets/img/logo.png"
 import React from 'react'
 import Link from 'next/link'
 import Button from './component/Button'
-import { usePathname } from 'next/navigation'
+import NavLink from './component/NavLink'
 const navLinks = [
     { name: "Home", href: "/" },
     { name: "About", href: "/about" },
     { name: "Work", href: "/work" },
-    { name: "Contact Us", href: "/contact" },
+    { name: "Contact", href: "/contact" },
 ]
 const Header = () => {
-    const pathname = usePathname()
     return (
         <header>
             <div className="container flex">
-                <Link href="/"><Image src={logo} alt='logo' width="100" /></Link>
+                <Link href="/" className='logo'>Ayush</Link>
                 <Button exCla="btn-hamburger" ariaExpanded={false}>
                     <span className="line"></span><span className="line"></span><span className="line"></span>
                 </Button>
                 <nav className="site-nav">
                     <ul className="site-nav-list">
                         {navLinks.map((navLink: any, id: number) => {
-                            return <li key={id} className={`site-nav__item ${pathname === navLink.href ? 'site-nav__active' : ''}`}> <Link className="site-nav__anchor" href={navLink.href}>{navLink.name}</Link></li>
+                            return <NavLink key={id} href={navLink.href} label={navLink.name} />
                         })}
                     </ul>
                 </nav>
