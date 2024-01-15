fba90fe2078329ff4528ad2987abc1097977506d
shahayush480 <shahayush480@gmail.com>
2024-01-15 21:49:10 +0530

    v3

--- app/component/Button.tsx
+++ app/component/Button.tsx
@@ -1,5 +1,6 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation';
 interface buttonParams {
     children: React.ReactNode,
     exCla?: string,
@@ -7,10 +8,14 @@ interface buttonParams {
     label: string
 }
 const Button = ({ children, exCla, ariaExpanded = false, label }: buttonParams) => {
+    const pathname = usePathname();
     let [aria, setAria] = useState(ariaExpanded);
     let handleClick = () => {
         setAria(!aria)
     }
+    useEffect(() => {
+        setAria(false)
+    }, [pathname])
     return (
         <button className={`btn ${exCla}`} aria-expanded={aria} aria-label={label} onClick={handleClick}>{children}</button>
     )
