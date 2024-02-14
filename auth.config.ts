8db5a861501ab791fa0adb944ee9ae831be74e03
shahayush480 <shahayush480@gmail.com>
2024-02-14 20:37:06 +0530

    app

--- auth.config.ts
+++
@@ -1,7 +0,0 @@
-import GitHub from "next-auth/providers/github";
-
-import type { NextAuthConfig } from "next-auth";
-
-export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig;
--- routes.ts
+++
@@ -1,2 +0,0 @@
-export const publicRoutes = ["/"];
-export const authRoutes = ["/admin/login"];
