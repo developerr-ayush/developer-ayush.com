ed45791edc94199c39ebc399a1e05f46b25bbdbe
shahayush480 <shahayush480@gmail.com>
2024-03-14 07:41:14 +0530

    jkdshjksd

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: ["*"],
   },
+  env: {
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
+  },
   webpack: (config, options) => {
     /**
      * Force scss source maps for debugging. If there are performance issues or you don't need debug css, use the value "eval-source-map" instead.
