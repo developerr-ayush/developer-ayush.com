b70b51f768863c6556f92dac5ebb94d5bac9ac53
Ayush shah <ayushshah@Ayushs-MacBook-Pro.local>
2024-06-29 12:22:06 +0530

    chore: Add lazy loading for images in Blog component

--- component/Blog.tsx
+++ component/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import CardList from '@/component/card/CardList'
 import Title from '@/component/Title'
 import BasicCard from '@/component/card/BasicCard'
@@ -34,29 +34,32 @@ const Blog = async () => {
         <div>No data found</div>
     )
     return (
-        <CardList className='grid grid-lg-3 align-start card-list-wrap '>
-            <div className="col-lg-3">
-                <Title>Blogs</Title>
-            </div>
-            <CardList className='col-lg-3 grid-lg-3 align-start'>
-                {data.map((article: Article) =>
-                    <div key={article.id}>
-                        <BasicCard img={{
-                            src: article.banner,
-                            alt: article.title,
-                            height: 500
-                        }}
-                            content={{
-                                title: article.title,
-                                text: article.description,
+        <Suspense fallback={<div>loading...</div>}>
+            <CardList className='grid grid-lg-3 align-start card-list-wrap '>
+                <div className="col-lg-3">
+                    <Title>Blogs</Title>
+                </div>
+                <CardList className='col-lg-3 grid-lg-3 align-start'>
+                    {data.map((article: Article) =>
+                        <div key={article.id}>
+                            <BasicCard img={{
+                                src: article.banner,
+                                alt: article.title,
+                                height: 500
                             }}
-                            redirect={`/blog/${article.id}`}
-                            redirectTitle={article.title}
-                            className="card-blog card-blog-list" />
-                    </div>
-                )}
+                                content={{
+                                    title: article.title,
+                                    text: article.description,
+                                }}
+                                redirect={`/blog/${article.id}`}
+                                redirectTitle={article.title}
+                                className="card-blog card-blog-list" />
+                        </div>
+                    )}
+                </CardList>
             </CardList>
-        </CardList>
+        </Suspense>
+
     )
 }
 
