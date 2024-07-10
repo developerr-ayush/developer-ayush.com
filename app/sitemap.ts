5eae88c689f88230911419f66407feeeb9dedb15
Ayush shah <ayushshah@Ayushs-MacBook-Pro.local>
2024-07-10 07:13:18 +0530

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,63 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap() {
+  const apiData = await fetch("https://auth-sigma-two.vercel.app/api/blog", {
+    next: {
+      revalidate: 3600,
+    },
+  });
+  let data = await apiData.json();
+  data = data.map((article: any) => {
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${article.id}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    };
+  });
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/work`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/credentials`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/services`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    ...data,
+  ];
+}
