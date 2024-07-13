f7e6653db0aa36b307e2d302aa8222d3b840b6e2
Ayush shah <ayushshah@Ayushs-MacBook-Pro.local>
2024-07-13 16:23:09 +0530

    removed suspense

--- component/Share.tsx
+++ component/Share.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 import { FaShare } from 'react-icons/fa'
 
 export const Share = ({ title }: { title: string }) => {
-    let shareData = {
-        title: title,
-        text: 'Check out this blog',
-        url: window.location.href
-    }
+
     return (
         <div className="blog-social">
             <button className="social-link" aria-label='share button' onClick={async () => {
+                let shareData = {
+                    title: title,
+                    text: 'Check out this blog',
+                    url: window.location.href
+                }
                 if (navigator.share && navigator.canShare(shareData)) {
                     let data = await navigator.share(shareData)
                 }
