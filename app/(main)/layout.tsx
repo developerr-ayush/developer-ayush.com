b1e0f370224be899d80f1a49bbf08a4b1f595c30
Ayush shah <ayushshah@Ayushs-MacBook-Pro.local>
2024-08-10 09:03:21 +0530

    defaulting

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -18,14 +18,7 @@ export const metadata: Metadata = {
     title: 'Developer Ayush',
     description: 'Created using NextJs by Developer Ayush',
     siteName: 'Developer Ayush',
-    images: [
-      {
-        url: 'https://www.developer-ayush.com/assets/img/default-share.png',
-        width: 800,
-        height: 600,
-        alt: 'Developer Ayush'
-      }
-    ]
+    images: 'https://www.developer-ayush.com/assets/img/default-share.png'
   },
   twitter: {
     title: 'Developer Ayush',
